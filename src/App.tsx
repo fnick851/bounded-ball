9d6236c86bb95d9ac8305e1fa6f04d25634ff4a6
Noah <hanlun.song@gmail.com>
2021-09-04 15:04:39 -0400

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,105 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import 'regenerator-runtime/runtime'
+import { World, Item } from 'react-dom-box2d'
+import { useRef, useState } from 'react'
+import { useCss } from 'kremling'
+import { useWindowSize, useMouse } from 'react-use'
+import { random, round } from 'lodash'
 
-function App() {
-  const [count, setCount] = useState(0)
+//degrees to radians
+const toRadians = function (degrees: number) {
+  return (degrees * Math.PI) / 180
+}
+const forceAtAngle = (force: number, angle: number) => {
+  const rad = toRadians(angle)
+  const x = force * Math.cos(rad)
+  const y = force * Math.sin(rad)
+  return [x, y]
+}
+
+const App = () => {
+  const cssScope = useCss(css)
+
+  const { width, height } = useWindowSize()
+  const ref = useRef(null)
+  const { docX, docY } = useMouse(ref)
+
+  const [cannonBalls, setCannonBalls] = useState<{ force: number[] }[]>([])
+  const [angle, setAngle] = useState<number>(45)
+  const makeCannonBall = () => {
+    setCannonBalls([{ force: forceAtAngle(100, angle) }])
+    const ang = random(0, 360, true)
+    setAngle(ang)
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div {...cssScope} className="root" ref={ref} onClick={makeCannonBall}>
+      <World width={width} height={height} gravity={[0, 0]}>
+        {cannonBalls.map((ball, i) => (
+          <Item
+            key={ball.force[1]}
+            left={docX}
+            top={docY}
+            density={1}
+            friction={0}
+            restitution={1}
+            width={30}
+            height={30}
+            initialForce={ball.force}
+            shape="circle"
           >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+            <div className="cannon-ball" />
+          </Item>
+        ))}
+        {/* reference line to visualize the angle */}
+        <div
+          className="line"
+          style={{
+            top: docY,
+            left: docX,
+            backgroundColor: 'black',
+          }}
+        ></div>
+        <div
+          className="line"
+          style={{
+            top: docY,
+            left: docX,
+            transform: `rotate(${toRadians(angle)}rad)`,
+            backgroundColor: 'green',
+          }}
+        >
+          <span>{round(angle, 1)}°</span>
+        </div>
+      </World>
     </div>
   )
 }
+
+const css = /*css*/ `
+* {
+  margin: 0;
+  padding: 0;
+}
+
+& .root {
+  cursor: pointer;
+}
+
+& .line {
+  position: absolute;
+  height: 2px;
+  width: 50px;
+  transform-origin: left center;
+  text-align: right;
+}
+
+& .line span {
+  font-size: 10px;
+}
+
+& .cannon-ball {
+  background-color: hotpink;
+}
+`
 
 export default App
---
+++ src/react-dom-2d.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-dom-box2d' {
+  const lib: { World: any; Item: any }
+  export = lib
+}
