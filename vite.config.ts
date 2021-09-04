fba2d9d062cc7ed7db48704b872ca05467b76fa6
Noah <hanlun.song@gmail.com>
2021-09-04 15:16:41 -0400

    temp page

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import reactJsx from 'vite-react-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), reactJsx()],
+  build: {
+    outDir: './docs',
+  },
+  base: '/bounded-ball/',
 })
